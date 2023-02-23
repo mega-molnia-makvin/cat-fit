cdf2b7a69adbf25bc46d3ed65e7493935b0479e1
mega-molnia-makvin <alinasilakova12322@gmail.com>
2023-02-23 20:44:56 +0300

    not the end of our team and cards, unfucking comments

--- scripts/main.js
+++ scripts/main.js
@@ -11,9 +11,12 @@ buttonClose.addEventListener("click",function(){
 })
 
 
-const buttonContinue=document.querySelector("#Continue");
+const buttonContinue=document.querySelector('"#Continue"');
+const labelNumber=document.querySelector('[name="login"]');
+const labelPas=document.querySelector('[name="password"]');
+
 buttonContinue.addEventListener("click",function(){
-if (true) {
+if (labelNumber!=null && labelPas!=null) {
     window.location.href = 'office.html';
   }
 })
