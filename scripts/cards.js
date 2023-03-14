12614840ce273598a9a020520ed98b3f91b5c769
mega-molnia-makvin <alinasilakova12322@gmail.com>
2023-03-14 22:01:07 +0300

    i am so sad and stupid crycrycry

--- scripts/cards.js
+++ scripts/cards.js
@@ -1,3 +1,6 @@
+const { on, click } = require("dom7");
+const { default: Framework7 } = require("framework7/.");
+
 const butOpen=document.querySelector('[data-modal_butt="buyBut"]');
 const modalBuy=document.querySelector('[data-modal="modalBuy"]');
 const butClose=document.querySelector('[data-modal_butt="closeBut"]');
@@ -12,44 +15,38 @@ butClose.addEventListener("click",function(){
 
 
 //calendar
+
 function datepicker(){
-    var calendar = app.calendar.create(rangePickerMaxdays(0));
-
+	var calendarMultiple = $f7.calendar.create({
+        inputEl: '#datepicker-date-range',
+        dateFormat: { month: 'short', day: 'numeric' },
+        multiple: true
+      });
 }
 
-//calendar
-    $.datepicker.regional['ru'] = {
-		closeText: 'Закрыть',
-		prevText: '&#x3c;Пред',
-		nextText: 'След&#x3e;',
-		currentText: 'Сегодня',
-		monthNames: ['Январь','Февраль','Март','Апрель','Май','Июнь', 'Июль','Август','Сентябрь','Октябрь','Ноябрь','Декабрь'],
-		monthNamesShort: ['Янв','Фев','Мар','Апр','Май','Июн', 'Июл','Авг','Сен','Окт','Ноя','Дек'],
-		dayNames: ['воскресенье','понедельник','вторник','среда','четверг','пятница','суббота'],
-		dayNamesShort: ['вск','пнд','втр','срд','чтв','птн','сбт'],
-		dayNamesMin: ['Вс','Пн','Вт','Ср','Чт','Пт','Сб'],
-		dateFormat: 'dd.mm.yy',
-		firstDay: 1,
-		isRTL: false,
-		showOtherMonths:true,
-		selectOtherMonths:true,
-		changeMonth:true,
-        changeYear: false,
-        showAnim:'scale'
-};
-var arr_data = [];
-$.datepicker.setDefaults($.datepicker.regional['ru']);
-$( '#datepicker' ).datepicker({ altField: "#show", altFormat: "yy-mm-dd",
-beforeShowDay: function(date) {
-for (var i=0; i<arr_data.length; i++)  { var d = arr_data[i];
-d = d.split('.');
-if (date.getTime() == (new Date(d[2],d[1]-1,d[0])).getTime()) return[true, "active", "Важная дата"] };
-return[true, "test"]},
-onSelect : function(value,b) {
-  var ok = $.inArray(value, arr_data) ;
-  if (ok == -1) arr_data.push(value)
-  else {arr_data.splice(ok,1)};
-  alert(JSON.stringify(arr_data));
-}
-})
-  
+//ебаный индус 
+var datepickerhhh = new function(){
+	var myApp=new Framework7();
+	var $$ =Dom7;
+	this.initializeApp=function(){
+	var monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August' , 'September' , 'October', 'November', 'December'];
+                           var datepicker = app.calendar.create({
+					inputEl: '#datepicker-date-range',
+					dateFormat: 'MM dd yyyy',
+					rangePicker: true,
+                    multiple: true,
+					closeByBackdropClick: true,
+                    
+				});
+				on:{
+					$$('#getDateBtn').on('click', function(){
+					myApp.alert(calendar.value);
+				})
+				$$('#setDateBtn').on('click', function(){
+					var dates=[new Date('2023-10-21'), new Date('2023-10-22'), new Date('2023-10-23')]
+					calendar.setValue(dates);
+				});
+}}}
+
+d=new datepicker();
+d.initializeApp();
