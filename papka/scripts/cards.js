3de612a10bae63eaf2b38989519c5c139fb0c3b2
BaoChi-1 <jelvira02@mail.ru>
2023-04-11 21:38:24 +0300

    Сделала кнопки, календарь всё ещё неоч хорошо работает

--- papka/scripts/cards.js
+++ papka/scripts/cards.js
@@ -1,11 +1,18 @@
-const butOpen=document.querySelector('[data-modal_butt="buyBut"]');
+const butOpen1=document.querySelector('[data-modal_butt="buyBut1"]');
+const butOpen2=document.querySelector('[data-modal_butt="buyBut2"]');
+const butOpen3=document.querySelector('[data-modal_butt="buyBut3"]');
 const modalBuy=document.querySelector('[data-modal="modalBuy"]');
 const butClose=document.querySelector('[data-modal_butt="closeBut"]');
 
-butOpen.addEventListener("click",function(){
+butOpen1.addEventListener("click",function(){
     modalBuy.classList.remove('content-hidden')
 })
-
+butOpen2.addEventListener("click",function(){
+    modalBuy.classList.remove('content-hidden')
+})
+butOpen3.addEventListener("click",function(){
+    modalBuy.classList.remove('content-hidden')
+})
 butClose.addEventListener("click",function(){
     modalBuy.classList.add('content-hidden')
 })
