a189f84891fc53018fe223b21cfd608e3c5e7b07
Ladybug1619 <youladybug16@gmail.com>
2023-05-03 16:26:54 +0300

    Логика фронта
    еще бы понять как сделать эту тупую связь, так как в видосике по  другому

---
+++ client2/src/store/sportStore.js
@@ -0,0 +1,42 @@
+import {makeAutoObservable} from "mobx";
+import { name } from "webix";
+
+export default class SportStore {
+    constructor(){
+        this._types = [
+            {id: 1, name: 'sport'},
+            {id:2, name: 'card'}
+        ]
+        this._price =[
+            {id:1, name: 'yoga', price:500},
+            {id:2, name: 'hula', price:400},
+            {id:3, name: 'hula', price:400},
+            {id:4, name: 'hula', price:400},
+            {id:5, name: 'hula', price:400}
+        ]
+        this._master=[
+            {id:1, name: 'lapka'},
+            {id:2, name: 'lapka'}
+        ]
+        makeAutoObservable(this)
+    }
+    setTypes(types){
+        this._types=types
+    }
+    setPrice(price){
+        this._price=price
+    }
+    setMaster(master){
+        this._master=master
+    }
+
+    get types(){
+        return this._yupes
+    }
+    get price(){
+        return this._price
+    }
+    get master(){
+        return this._master
+    }
+}
