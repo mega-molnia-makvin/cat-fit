2b6b4eba28ce4b103731f29b1df8a5be0e65b402
NASTYA-153 <116902989+NASTYA-153@users.noreply.github.com>
2023-05-26 12:12:49 +0300

    ор

--- papka/scripts/datepicker.js
+++ papka/scripts/datepicker.js
@@ -16,3 +16,21 @@ new AirDatepicker('#date', {
     minutesStep: 5,
     dateFormat:'d.MM'
 })
+new AirDatepicker('#cal', {
+    inline:true,
+    multipleDates: true,
+    timepicker: true,
+    minHours: 9,
+    maxHours: 18,
+    minutesStep: 5,
+    dateFormat:'d.MM'
+})
+new AirDatepicker('#cal2', {
+    inline:true,
+    multipleDates: true,
+    timepicker: true,
+    minHours: 9,
+    maxHours: 18,
+    minutesStep: 5,
+    dateFormat:'d.MM'
+})
