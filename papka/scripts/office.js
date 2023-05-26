4406d45ac45a8dc1eb0d7a1824057912b34ac89e
BaoChi-1 <jelvira02@mail.ru>
2023-05-26 10:45:14 +0300

    Доделаем мод.окно на паре

---
+++ papka/scripts/office.js
@@ -0,0 +1,3 @@
+const butEx=document.querySelector('[data-modal_butt="btn-ex"]');
+const butadd=document.querySelector('[data-modal_butt="btn-add"]');
+const modalBuy=document.querySelector('[data-modal="modal_offers"]');
