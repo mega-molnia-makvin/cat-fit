3849ba2db2048315b7a12d09867bc0a0193bc76d
BaoChi-1 <jelvira02@mail.ru>
2023-06-04 21:32:33 +0300

    Починила всё,кроме модальных окон.Завтра будем думать

--- server/ejs-views/scripts/office.js
+++ server/ejs-views/scripts/office.js
@@ -1,27 +1,19 @@
 const butEx=document.querySelector('[data-modal_butt="btn-ex"]');
 const modalOf=document.querySelector('[data-modal="modal_con"]');
 const butclose2=document.querySelector('[data-modal_butt="btn-close-con"]');
+const month=document.querySelector('.month1');
+const year=document.querySelector('.year1');
+const hal=document.querySelector('.half1');
 
+const startDateInput = document.getElementById('start-date');
+const endDateOutput = document.getElementById('end-date');
+endDateOutput.value='';
 
-
-
-
-butEx.addEventListener("click",function(){
-    modalOf.classList.remove('content-hidden');
-})
-butclose2.addEventListener("click",function(){
-    modalOf.classList.add("content-hidden")
-})
-document.querySelector('.cont').addEventListener('click',()=>{
-    let month=document.querySelector('.month1');
-    let half=document.querySelector('.year1');
-    const startDateInput = document.getElementById('start-date');
-    const endDateOutput = document.getElementById('end-date');
-
-    if (month.checked){
 startDateInput.addEventListener('change', function() {
-    endDateOutput.textContent='';
-const selectedDate = new Date(this.value);
+    var valid=false
+    if(month.checked){
+        valid=true;
+        const selectedDate = new Date(this.value);
 selectedDate.setMonth(selectedDate.getMonth() + 1);
 
 const endDate = selectedDate.toLocaleDateString('en-US', {
@@ -31,37 +23,43 @@ day: 'numeric'
 });
 
 endDateOutput.value = `${endDate}`;
-});
     }
     if(half.checked){
-        startDateInput.addEventListener('change', function() {
-            endDateOutput.textContent='';
-            const selectedDate = new Date(this.value);
-            selectedDate.setMonth(selectedDate.getMonth() + 6);
-            
-            const endDate = selectedDate.toLocaleDateString('en-US', {
-            year: 'numeric',
-            month: 'numeric',
-            day: 'numeric'
-            });
-            
-            endDateOutput.value = `${endDate}`;
-            });
+        valid=true;
+        const selectedDate = new Date(this.value);
+selectedDate.setMonth(selectedDate.getMonth() + 6);
+
+const endDate = selectedDate.toLocaleDateString('en-US', {
+year: 'numeric',
+month: 'numeric',
+day: 'numeric'
+});
+
+endDateOutput.value = `${endDate}`;
     }
-    else{
-        startDateInput.addEventListener('change', function() {
-            endDateOutput.textContent='';
-            const selectedDate = new Date(this.value);
-            selectedDate.setMonth(selectedDate.getMonth() + 12);
-            
-            const endDate = selectedDate.toLocaleDateString('en-US', {
-            year: 'numeric',
-            month: 'numeric',
-            day: 'numeric'
-            });
-            
-            endDateOutput.value = `${endDate}`;
-            });
+    if(year.checked){
+        valid=true;
+        const selectedDate = new Date(this.value);
+selectedDate.setMonth(selectedDate.getMonth() + 12);
+
+const endDate = selectedDate.toLocaleDateString('en-US', {
+year: 'numeric',
+month: 'numeric',
+day: 'numeric'
+});
+
+endDateOutput.value = `${endDate}`;
     }
-    
+    if(valid==false){
+        alert("Выберете что-нибудь")
+    }
+});
+
+
+
+butEx.addEventListener("click",function(){
+    modalOf.classList.remove('content-hidden');
+})
+butclose2.addEventListener("click",function(){
+    modalOf.classList.add("content-hidden")
 })
