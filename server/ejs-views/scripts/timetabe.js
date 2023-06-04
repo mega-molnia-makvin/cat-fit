649730884fc305ab1d4f6f6a05601c5639b661d2
BaoChi-1 <jelvira02@mail.ru>
2023-06-04 11:25:38 +0300

    Не поняла,Как добавить модальные окна в другие кнопки.Пока добавила в 7

--- server/ejs-views/scripts/timetabe.js
+++ server/ejs-views/scripts/timetabe.js
@@ -3,6 +3,8 @@ const butOpen2=document.getElementById('2');
 const butOpen3=document.getElementById('3');
 const butOpen4=document.getElementById('4');
 const butOpen5=document.getElementById('5');
+const butOpen6=document.getElementById('6');
+const butOpen7=document.getElementById('7');
 const modalBuy=document.querySelector('[data-modal="modal0"]');
 const butClose=document.querySelector('[data-modal_butt="modal2"]');
 butOpen1.addEventListener("click",function(){
@@ -18,6 +20,12 @@ butOpen4.addEventListener("click",function(){
     modalBuy.classList.remove('content-hidden')
 })
 butOpen5.addEventListener("click",function(){
+    modalBuy.classList.remove('content-hidden')
+})
+butOpen6.addEventListener("click",function(){
+    modalBuy.classList.remove('content-hidden')
+})
+butOpen7.addEventListener("click",function(){
     modalBuy.classList.remove('content-hidden')
 })
 butClose.addEventListener("click",function(){
